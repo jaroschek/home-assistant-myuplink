49d247a2345ef401aa10508f27ef583591d2220d
Stefan Jaroschek <stefan@jaroschek.org>
2024-08-30 09:11:34 +0200

    Split config flow to move new parameter options into optional expert step

--- custom_components/myuplink/const.py
+++ custom_components/myuplink/const.py
@@ -31,6 +31,7 @@ PLATFORMS = [
 ]
 
 CONF_ADDITIONAL_PARAMETER = "additional_parameter"
+CONF_EXPERT_MODE = "expert_mode"
 CONF_FETCH_FIRMWARE = "fetch_firmware"
 CONF_FETCH_NOTIFICATIONS = "fetch_notifications"
 CONF_PARAMETER_WHITELIST = "parameter_whitelist"
